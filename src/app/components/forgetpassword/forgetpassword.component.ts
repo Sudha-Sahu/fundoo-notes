a4dc96ea4b12c7beec044975171d68d7ed831d3e
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-27 21:29:45 +0530

    [Sudha]-refactored the code by adding routing and validations in forget password

--- src/app/components/forgetpassword/forgetpassword.component.ts
+++ src/app/components/forgetpassword/forgetpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgetpassword',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgetpassword.component.scss']
 })
 export class ForgetpasswordComponent implements OnInit {
+  forgetForm !: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.forgetForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    })
   }
 
 }
