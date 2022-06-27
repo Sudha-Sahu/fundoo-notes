3aaba40a6087a138852035595d855aadb598e4f2
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-27 21:28:58 +0530

    [Sudha]-refactored the code by adding routing and validations in login

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  loginForm !: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log("Valid data", this.loginForm.value);
+    }
+    else{
+      console.log("Invalid data", this.loginForm.value);
+    }
   }
 
 }
