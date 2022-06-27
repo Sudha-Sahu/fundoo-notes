e1e9572b7a832772dc348bbb5cc7bdc21dbef3a7
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-27 21:15:39 +0530

    [Sudha]-refactored the code by adding routing and validations

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { validateBasis } from '@angular/flex-layout';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userservice/user.service';
+
+// import { MustMatch } from './_helpers/must-match.validator';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  registerForm !: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder, private user:UserService) { 
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(8)]],
+      cpass: ['', Validators.required],
+      service: ['advance']
+    })
+  }
+
+
+  onSubmit(){
+    if(this.registerForm.valid){
+      console.log("Valid data", this.registerForm.value);
+      let data = {
+        firstName: this.registerForm.value.fname,
+        lastName: this.registerForm.value.lname,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.pass,
+        service: "advance"
+      }
+      this.user.register(data).subscribe((result:any)=>{console.log(result);})
+    }
+    else{
+      console.log("Invalid data", this.registerForm.value);
+    }
+  }
 }
