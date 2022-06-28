7d01ecf611b64c0d0e6d50f2a80bc2d59651bba5
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-28 13:49:51 +0530

    [Sudha]-refactored the code by adding api calling in resetpass components

--- src/app/components/resetpassword/resetpassword.component.ts
+++ src/app/components/resetpassword/resetpassword.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/userservice/user.service';
 
 @Component({
   selector: 'app-resetpassword',
@@ -8,14 +10,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ResetpasswordComponent implements OnInit {
   resetForm !: FormGroup;
+  token:any;
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder, private user:UserService, private reset:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.resetForm = this.fb.group({
       pass: ['', [Validators.required, Validators.minLength(8)]],
-      cpass: ['', Validators.required]
+      cpass: ['', Validators.required],
+      service: ['advance']
     })
+    this.token = this.reset.snapshot.params['token'];
+    console.log(this.token)
   }
-
+  onSubmit(){
+    if(this.resetForm.valid){
+      console.log("Valid data", this.resetForm.value);
+      let data = {
+        newPassword: this.resetForm.value.pass
+      }
+      this.user.resetpass(data, this.token).subscribe((result:any)=>{console.log(result);})
+    }
+    else{
+      console.log("Invalid data", this.resetForm.value);
+    }
+  }
 }
