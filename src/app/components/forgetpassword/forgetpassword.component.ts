7cc4f282918df9d91ddd0bfba817c47a236b5a3a
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-28 13:48:53 +0530

    [Sudha]-refactored the code by adding api calling in forgetpass components

--- src/app/components/forgetpassword/forgetpassword.component.ts
+++ src/app/components/forgetpassword/forgetpassword.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userservice/user.service';
 
 @Component({
   selector: 'app-forgetpassword',
@@ -9,12 +10,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ForgetpasswordComponent implements OnInit {
   forgetForm !: FormGroup;
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder, private user:UserService) { }
 
   ngOnInit(): void {
     this.forgetForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
+      service: ['advance']
     })
   }
+  onSubmit(){
+    if(this.forgetForm.valid){
+      console.log("Valid data", this.forgetForm.value);
+      let data = {
+        email: this.forgetForm.value.email,
+      }
+      this.user.forget(data).subscribe((result:any)=>{console.log(result);})
+    }
+    else{
+      console.log("Invalid data", this.forgetForm.value);
+    }
+  }
+
 
 }
