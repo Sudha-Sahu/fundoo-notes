9f047b06873bc448e8c54a200d32ce87928ffab0
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-28 13:48:14 +0530

    [Sudha]-refactored the code by adding api calling in signin components

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userservice/user.service';
 
 @Component({
   selector: 'app-signin',
@@ -9,17 +10,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit {
   loginForm !: FormGroup;
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder, private user:UserService) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      pass: ['', [Validators.required, Validators.minLength(8)]]
+      pass: ['', [Validators.required, Validators.minLength(8)]],
+      service: ['advance']
     })
   }
   onSubmit(){
     if(this.loginForm.valid){
       console.log("Valid data", this.loginForm.value);
+      let data = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.pass,
+        service: this.loginForm.value.service
+      }
+      this.user.login(data).subscribe((result:any)=>{console.log(result);})
     }
     else{
       console.log("Invalid data", this.loginForm.value);
