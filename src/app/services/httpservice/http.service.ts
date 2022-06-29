7fda26fe73e969d678d0cfa66882a3d8062f8b48
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-29 15:45:09 +0530

    [Sudha]-created services http and user for api calling

---
+++ src/app/services/httpservice/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  BaseURL= environment.baseUrl;
+
+  constructor(private httpclient:HttpClient) { }
+
+  postService(url:string, reqdata:any, token:boolean=false, httpOptions:any){
+
+    console.log(reqdata);
+    return this.httpclient.post(this.BaseURL+url, reqdata, token && httpOptions)
+  }
+
+  getService(url:string, token:boolean=false, httpOptions:any){
+    return this.httpclient.get(url, token && httpOptions)
+  }
+
+  putService(){}
+
+  
+  deleteService(){}
+}
---
+++ src/app/services/userservice/user.service.ts
@@ -0,0 +1,66 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpService } from '../httpservice/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpService) { }
+
+  register(data:any){
+    console.log(data);
+
+    let header={
+      headers: new HttpHeaders({
+        'Content-type': 'application/json'
+      })
+    }
+    return this.http.postService('user/userSignUp', data, false, header)
+  }
+
+  login(data:any){
+    console.log("login details api", data);
+
+    let header={
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      })
+    }
+    return this.http.postService('user/login', data, false, header)
+  }
+  forget(data:any){
+    console.log("forget details api", data);
+
+    let header={
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      })
+    }
+    return this.http.postService('user/reset', data, false, header)
+  }
+  encode(data: any) {
+    const formBody = [];
+    for (const property in data) {
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(data[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    return formBody.join('&');
+  }
+
+  resetpass(data:any, token:any){
+    console.log("reset details", data, token);
+
+    let header={
+      headers: new HttpHeaders({
+        'Content-type': 'application/x-www-form-urlencoded',
+        'Authorization': token
+      })
+    }
+    return this.http.postService('user/reset-password', this.encode(data), true, header)
+  }
+}
