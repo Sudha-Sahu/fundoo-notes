7ee07fb2a3930714c03cca34a11479efea25e485
Sudha Sahu <sudhasahu52@gmail.com>
2022-06-30 10:32:58 +0530

    [Sudha]-refactored the code in UI design of fundoo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreatenoteComponent } from './components/createnote/createnote.component';
+import { DisplayNotesComponent } from './components/display-notes/display-notes.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
+import { IconsComponent } from './components/icons/icons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signup', component:SignupComponent},
+  {path: 'signin', component:SigninComponent},
+  {path: 'resetpassword/:token', component:ResetpasswordComponent},
+  {path: 'forgetpass', component:ForgetpasswordComponent},
+  {path: 'dashboard', component:DashboardComponent,
+    children: [
+      {path: 'createnotes', component:CreatenoteComponent},
+      {path: 'displaynotes', component:DisplayNotesComponent},
+      {path: 'notes', component:GetAllNotesComponent}
+    ]
+},
+{path: 'icon', component:IconsComponent},
+{path: 'getallnote', component:GetAllNotesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,11 +9,38 @@ import { MatInputModule } from '@angular/material/input';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SigninComponent } from './components/signin/signin.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreatenoteComponent } from './components/createnote/createnote.component';
+import { IconsComponent } from './components/icons/icons.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
+import { DisplayNotesComponent } from './components/display-notes/display-notes.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { TrashComponent } from './components/trash/trash.component';
+import { ArchiveComponent } from './components/archive/archive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    SigninComponent,
+    ResetpasswordComponent,
+    ForgetpasswordComponent,
+    DashboardComponent,
+    CreatenoteComponent,
+    IconsComponent,
+    GetAllNotesComponent,
+    DisplayNotesComponent,
+    TrashComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +50,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     FlexLayoutModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    ReactiveFormsModule, HttpClientModule,
+    MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
